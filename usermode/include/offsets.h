08bc5d2cc0923c53313df88c21c617dd3acff3e9
CombaticaBen00n <162020002+CombaticaBen00n@users.noreply.github.com>
2024-04-23 22:46:07 +0300

    separated offsets file

---
+++ usermode/include/offsets.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstddef>
+
+namespace offsets
+{
+    constexpr std::ptrdiff_t dwLocalPlayer = 0x17361C8;
+    constexpr std::ptrdiff_t dwEntityList = 0x18C1EA8;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x1923180;
+
+    constexpr std::ptrdiff_t m_iHealth = 0x334;
+    constexpr std::ptrdiff_t dwPlayerPawn = 0x7E4;
+    constexpr std::ptrdiff_t m_iTeamNum = 0x3CB;
+    constexpr std::ptrdiff_t m_vec_Origin = 0x127C;
+}
--- usermode/src/main.cpp
+++ usermode/src/main.cpp
@@ -1,30 +1,18 @@
 #include "memory.h"
 #include "helpers.h"
-#include <iostream>
-
+#include "render.h"
+#include "offsets.h"
 #include "vector.h"
 #include <dwmapi.h>
 #include <d3d11.h>
+#include <iostream>
 #include <windowsx.h>
+
 #include "../external/ImGui/imgui.h"
 #include "../external/ImGui/imgui_impl_dx11.h"
 #include "../external/ImGui/imgui_impl_win32.h"
-#include "render.h"
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-namespace offsets
-{
-    constexpr std::ptrdiff_t dwLocalPlayer = 0x17361C8;
-    constexpr std::ptrdiff_t dwEntityList = 0x18C1EA8;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x1923180;
-
-    constexpr std::ptrdiff_t m_iHealth = 0x334;
-    constexpr std::ptrdiff_t dwPlayerPawn = 0x7E4;
-    constexpr std::ptrdiff_t m_iTeamNum = 0x3CB;
-    constexpr std::ptrdiff_t m_vec_Origin = 0x127C;
-}
-
 
 LRESULT CALLBACK window_procedure(HWND window, UINT message, WPARAM w_param, LPARAM l_param)
 {

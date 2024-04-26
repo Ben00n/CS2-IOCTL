fb4ecdb502eedd1193abfeb05bf9ff2d11178099
CombaticaBen00n <162020002+CombaticaBen00n@users.noreply.github.com>
2024-04-26 13:33:21 +0300

    updated offsets

--- usermode/include/offsets.h
+++ usermode/include/offsets.h
@@ -3,12 +3,12 @@
 
 namespace offsets
 {
-    constexpr std::ptrdiff_t dwLocalPlayer = 0x17361C8;
-    constexpr std::ptrdiff_t dwEntityList = 0x18C1EA8;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x1923180;
+    constexpr std::ptrdiff_t dwLocalPlayer = 0x1915C18;
+    constexpr std::ptrdiff_t dwEntityList = 0x18C6268;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x19278B0;
 
     constexpr std::ptrdiff_t m_iHealth = 0x334;
     constexpr std::ptrdiff_t dwPlayerPawn = 0x7E4;
     constexpr std::ptrdiff_t m_iTeamNum = 0x3CB;
-    constexpr std::ptrdiff_t m_vec_Origin = 0x127C;
+    constexpr std::ptrdiff_t m_vec_Origin = 0x80;
 }

67fe3410aedfd7cd16d635108c392c9fa42ecf7c
CombaticaBen00n <162020002+CombaticaBen00n@users.noreply.github.com>
2024-05-10 12:15:29 +0300

    fixed vs setting error, updated offsets.

--- usermode/include/offsets.h
+++ usermode/include/offsets.h
@@ -3,9 +3,9 @@
 
 namespace offsets
 {
-    constexpr std::ptrdiff_t dwLocalPlayer = 0x1917698;
-    constexpr std::ptrdiff_t dwEntityList = 0x18C7F88;
-    constexpr std::ptrdiff_t dwViewMatrix = 0x1929420;
+    constexpr std::ptrdiff_t dwLocalPlayer = 0x19176A8;
+    constexpr std::ptrdiff_t dwEntityList = 0x18C7F98;
+    constexpr std::ptrdiff_t dwViewMatrix = 0x1929430;
 
     constexpr std::ptrdiff_t m_iHealth = 0x334;
     constexpr std::ptrdiff_t dwPlayerPawn = 0x7E4;
